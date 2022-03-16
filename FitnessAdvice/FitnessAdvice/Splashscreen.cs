b884afc498f16eba2fa83797ee9a7796345723e6
janosheen <91180541+janosheen@users.noreply.github.com>
2022-03-16 09:28:01 +0100

    Rozrobený spleš skrín :)

---
+++ FitnessAdvice/FitnessAdvice/Splashscreen.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace FitnessAdvice
+{
+    public class Splashscreen : ContentPage 
+    {
+        Image splashscreenImage;
+        public Splashscreen()
+        {
+            splashscreenImage = new Image()
+            {
+                Source = "starting-screen.png"
+            };
+        }
+    }
+}
