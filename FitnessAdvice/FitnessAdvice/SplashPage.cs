3386868dc3fcbb6811d5562ab0193c42251ced08
janosheen <91180541+janosheen@users.noreply.github.com>
2022-03-28 18:49:15 +0200

    Add project files.

---
+++ FitnessAdvice/FitnessAdvice/SplashPage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace FitnessAdvice
+{
+    public class SplashPage : ContentPage
+    {
+        public Image splashImage;
+
+        public SplashPage()
+        {
+            NavigationPage.SetHasNavigationBar(this, false);
+            Image splashImage = new Image
+            {
+                Source = "/Resources/drawable/startingscreen.png" //"startingscreen.png"
+            };
+
+        }
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            System.Threading.Thread.Sleep(250);
+            Application.Current.MainPage = new NavigationPage(new FirstAppears());
+        }
+    }
+}
