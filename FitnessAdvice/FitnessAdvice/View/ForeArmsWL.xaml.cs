bafbae1f133e3f8cbb620f5405b3d6a41b932836
yanosheen <triggered71@gmail.com>
2022-05-27 08:51:35 +0200

    Second WL exercise ! Third one is on the way!

---
+++ FitnessAdvice/FitnessAdvice/View/ForeArmsWL.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FitnessAdvice.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ForeArmsWL : ContentPage
+    {
+        public ObservableCollection<Exercise> Tricepcirses { get; set; }
+        public ForeArmsWL()
+        {
+            InitializeComponent();
+
+            Tricepcirses = new ObservableCollection<Exercise>
+            {
+                new Exercise { Name = "Isometric wall push", Type = "Push", Image = "forearm_icon.png"}
+
+        };
+            lstview.ItemsSource = Tricepcirses;
+        }
+        private async void Back_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ArmsPartSelection_BW(), true);
+        }
+
+        private async void Isometric_wall_push_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new IsometricWallPush(), true);
+        }
+    }
+}
---
+++ FitnessAdvice/FitnessAdvice/View/ForearmPull.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FitnessAdvice.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ForearmPull : ContentPage
+    {
+        public ForearmPull()
+        {
+            InitializeComponent();
+        }
+
+        private void Back_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
